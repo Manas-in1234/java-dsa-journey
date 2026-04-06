741add7da2320495c6ab68b28c0e911a9d8dd2f2
Manas <kassetymanas123@gmail.com>
2026-04-06 20:22:34 +0530

    Day4 Progress

---
+++ Day4/OneToN.java
@@ -0,0 +1,11 @@
+package Day4;
+
+
+public class OneToN {
+    public static void main(String []args){
+    int n = 15;
+    for(int i = 1;i<n;i++){
+        System.out.println(i);
+    }
+}
+}
---
+++ Day4/SumOfNum.java
@@ -0,0 +1,11 @@
+
+public class SumOfNum {
+    public static void main(String []args){
+        int n = 15;
+        int sum =0;
+        for(int i = 1;i <= n;i++){
+            sum = sum+i;
+        }
+        System.out.println("The sum of numbers is:"+sum);
+    }
+}
